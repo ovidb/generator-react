fe599dc62f424d5b29d7c56f040f7b8263f67e7b
Ovidiu D.Bodea <antarexis@gmail.com>
2019-09-10 11:25:57 +0100

    Improved story

--- generators/app/templates/name.story.js
+++ generators/app/templates/name.story.js
@@ -2,14 +2,13 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, boolean } from '@storybook/addon-knobs/react';
 import { <%= nameUp %> } from './<%= name %>.jsx';
 import pageDecorator from '../../../.storybook/decorators/page-decorator';
-import className from 'classnames';
 
 storiesOf('<%= category %>| <%= nameUp %>', module)
   .addDecorator(withKnobs)
   .addDecorator(pageDecorator)
   .add('State 1', () => (
-
+    <<%= nameUp %> />
   ))
   .add('State 2', () => (
-
+    <<%= nameUp %> />
   ));
